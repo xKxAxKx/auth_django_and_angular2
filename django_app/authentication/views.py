6bb0c648afc287b9b38310f8f850ceefa88f17da
xKxAxKx <pekepooooong@gmail.com>
2017-06-11 16:06:00 +0900

    viewの修正

--- django_app/authentication/views.py
+++ django_app/authentication/views.py
@@ -3,6 +3,7 @@ from rest_framework_jwt.settings import api_settings
 from rest_framework.generics import GenericAPIView
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 from rest_framework import status
 from rest_framework.views import APIView
@@ -16,14 +17,18 @@ jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 class AuthRegister(APIView):
     serializer_class = AccountSerializer
+    permission_classes = (AllowAny,)
 
-    def post(self, request):
+    def post(self, request, format=None):
         serializer = AccountSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = authenticate(**serializer.data)
-        if not user:
-            raise AuthenticationFailed()
-        payload = jwt_payload_handler(user)
-        return Response({
-            'token': jwt_encode_handler(payload),
-        })
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # user = authenticate(**serializer.data)
+        # if not user:
+        #     raise AuthenticationFailed()
+        # payload = jwt_payload_handler(user)
+        # return Response({
+        #     'token': jwt_encode_handler(payload),
+        # })
