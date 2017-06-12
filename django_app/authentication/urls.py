40c9900acae939721877638f84490272a9f69902
kakutani <pekepooooong@gmail.com>
2017-06-12 18:06:40 +0900

    url更新

--- django_app/authentication/urls.py
+++ django_app/authentication/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import include, url
 from .views import AuthRegister
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
-    url(r'^login/', obtain_jwt_token),
-    url(r'^token-refresh/', refresh_jwt_token),
-    url(r'^token-verify/', verify_jwt_token),
     url(r'^register/$', AuthRegister.as_view()),
 ]
--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/v1/auth/', include('authentication.urls')),
+    url(r'^login/', obtain_jwt_token),
+    url(r'^token-refresh/', refresh_jwt_token),
+    url(r'^token-verify/', verify_jwt_token),
+    url(r'^api/user/', include('authentication.urls')),
 ]
