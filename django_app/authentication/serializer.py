48842221995da7cf80b03fdc39e8f4119aee7d32
kakutani <pekepooooong@gmail.com>
2017-06-20 16:55:29 +0900

    serializer修正

--- django_app/authentication/serializer.py
+++ django_app/authentication/serializer.py
@@ -11,15 +11,3 @@ class AccountSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return Account.objects.create_user(request_data=validated_data)
-
-    def update(self, instance, validated_data):
-        if 'user' in validated_data:
-            user_instance = instance.user
-            user = validated_data.pop('user')
-            if 'email' in user:
-                user_instance.email = user['email']
-            if 'username' in user:
-                user_instance.username = user['username']
-            user_instance.save()
-        instance = super().update(instance, validated_data)
-        return instance
