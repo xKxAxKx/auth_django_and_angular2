3a0ac004a1d2d079f877e6dfabeab9ccea93fafa
xKxAxKx <pekepooooong@gmail.com>
2017-06-24 17:48:18 +0900

    [update]djangoにdelete用のAPI URLを作成

--- django_app/authentication/urls.py
+++ django_app/authentication/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from rest_framework import routers
-from .views import AuthRegister, UserListView, AuthInfoGetView, AuthInfoUpdateView
+from .views import AuthRegister, UserListView, AuthInfoGetView, AuthInfoUpdateView, AuthInfoDeleteView
 
 urlpatterns = [
     url(r'^user_info/$', UserListView.as_view()), #あとで消す
     url(r'^register/$', AuthRegister.as_view()),
     url(r'^mypage/$', AuthInfoGetView.as_view()),
     url(r'^auth_update/?$', AuthInfoUpdateView.as_view()),
+    url(r'^delete/?$', AuthInfoDeleteView.as_view()),
 ]
