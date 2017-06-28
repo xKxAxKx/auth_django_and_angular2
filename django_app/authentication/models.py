321ac49f865cf1451c67267e59fd834cb237b3eb
kakutani <pekepooooong@gmail.com>
2017-06-28 13:40:08 +0900

    [update]ユーザ削除実装済

--- django_app/authentication/models.py
+++ django_app/authentication/models.py
@@ -56,7 +56,6 @@ class Account(AbstractBaseUser):
     is_staff    = models.BooleanField(default=False)
     is_admin    = models.BooleanField(default=False)
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
-    delete      = models.BooleanField(default=0)
 
     objects = AccountManager()
 
