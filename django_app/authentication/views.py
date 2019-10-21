2100e924d317e8448a16b8b8ca9f229145a0bcd9
Hiromasa Kakutani <pekepooooong@gmail.com>
2019-10-21 14:40:00 +0900

    Update views.py

--- django_app/authentication/views.py
+++ django_app/authentication/views.py
@@ -1,14 +1,12 @@
 from django.contrib.auth import authenticate
+from django.db import transaction
+from django.http import HttpResponse, Http404
 from rest_framework import authentication, permissions, generics
 from rest_framework_jwt.settings import api_settings
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
-from django.db import transaction
-from django.http import HttpResponse, Http404
-
 from rest_framework import status, viewsets, filters
 from rest_framework.views import APIView
-
 from .serializer import AccountSerializer
 from .models import Account, AccountManager
 
